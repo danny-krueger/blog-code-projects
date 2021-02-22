1c744f90e4ec8c7567397d20a4318b0e028ae2c9
Danny Krüger <krueger.dk13@gmail.com>
2021-02-22 20:30:40 +0100

    Added dotnet 5.0 log4net project

---
+++ 001/log4netLogsToElasticsearch/log4netLogsToElasticsearch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+
+namespace log4netLogsToElasticsearch
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(nameof(Program));
+
+        static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+
+            log.Info("Test Test");
+        }
+    }
+}
